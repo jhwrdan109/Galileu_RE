c4b3d81a7c772350f3cac2b5aa90d314c3d1b04c
jhordan <daneken109@gmail.com>
2025-06-19 19:34:43 -0400

    final

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Meu Site Incrível</title>
+      <link rel="icon" href="images/favicon.ico" />
+      <meta name="description" content="Descrição do meu site aqui" />
+      {/* outras meta tags que desejar */}
+    </>
+  );
+}
